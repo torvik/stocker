d0907db2b08c26acf4a2c818921836dfd271ab0c
torvik <pless.victor@gmail.com>
2018-05-04 21:22:42 +0000

    stocker

--- Gemfile
+++ Gemfile
@@ -32,3 +32,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 
 gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
+
+gem 'stock_quote', '~> 2.0'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
 class HomeController < ApplicationController
   def index
+
+    if params[:id] == ""
+      @nothing = "Hey, you forgot to enter a symbol"
+    elsif 
+      if params[:id]
+        begin
+          @stock = StockQuote::Stock.quote(params[:id])
+        rescue StandardError
+          @error = "That Stock Symbol Doesn´t Exist... please try again."
+        end
+
+        
+      end
+    end
   end
 
   def about 
